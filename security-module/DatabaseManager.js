94f9cc0b002f93b2de5bdfc2c08dd62b1883d859
userq11 <unlimited5511@interia.pl>
2020-01-22 16:08:53 +0100

    singleton

---
+++ security-module/DatabaseManager.js
@@ -0,0 +1,28 @@
+// Singleton class
+
+const MySql = require("sync-mysql");
+
+class DatabaseManager {
+  instance = null;
+
+  getInstance() {
+    if (this.instance === null) {
+      this.instance = new DatabaseManager();
+    }
+
+    return this.instance;
+  }
+
+  getConnection() {
+    const connection = new MySql({
+      host: "michnamarcin.pl",
+      user: "DPuser",
+      password: "polska1",
+      database: "DPbase"
+    });
+
+    return connection;
+  }
+}
+
+module.exports = DatabaseManager;
